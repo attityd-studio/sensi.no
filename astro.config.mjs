9894c78a6d78b7233e2b85bd2f8bd80939206267
Alexander Prestmo <aprestmo@gmail.com>
2025-03-30 21:13:59 +0200

    feat(app): Update layout component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import aiRobotsTxt from 'astro-ai-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sensi.no',
-  integrations: [sitemap(), aiRobotsTxt()]
+  integrations: [sitemap(), aiRobotsTxt()],
+  env: {
+    schema: {
+      SITE_NAME: envField.string({ context: "server", access: "public", default: "Sensi" }),
+    }
+  }
 });
